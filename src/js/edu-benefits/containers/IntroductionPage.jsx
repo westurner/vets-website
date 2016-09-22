7f8abb3416adbc781602c0230f047dd3ffeae2cf
Jeff Balboni <jeff@adhocteam.us>
2016-09-22 10:45:57 -0400

    Intro page

--- src/js/edu-benefits/containers/IntroductionPage.jsx
+++ src/js/edu-benefits/containers/IntroductionPage.jsx
@@ -12,17 +12,18 @@ class IntroductionPage extends React.Component {
 
         <div className="row">
           <div className="small-12 columns">
-            <p>
-              Fill out this application with the most accurate information you have. The more accurate it is, the more likely you are to get a rapid response.
-            </p>
-            <p>
-              VA uses the information you submit to determine your eligibility and to provide you with the best service.
-            </p>
-            <p>
-              Federal law provides criminal penalties, including a fine and/or imprisonment for up to 5 years, for concealing a material fact or making a materially false statement. (See <a href="https://www.justice.gov/usam/criminal-resource-manual-903-false-statements-concealment-18-usc-1001" target="_blank">18 U.S.C. 1001</a>)
-            </p>
+            <p>Fill out as many blocks in the application as you can. Please be as specific as possible. VA uses this information to determine whether you are eligible for education benefits.</p>
+            <p>You will not be able to save your work or come back later to finish, so have this information handy when you begin:</p>
+            <ul className="edu-benefits-list">
+              <li>Paperwork related to your military, education, and employment history (such as your DD214)</li>
+              <li>Whether you are eligible for the specific education program you are applying for</li>
+              <li>Which school you want to attend and what you want to study</li>
+            </ul>
+            <p>This online application is based on VA Form 22-1990.</p>
             <div className="usa-alert usa-alert-info">
-              <strong>Note:</strong> You will not be able to save your progress once you have started the form.
+              <div className="usa-alert-body">
+                <strong>Note:</strong> According to federal law, there are criminal penalties, including a fine and/or imprisonment for up to 5 years, for withholding information or for providing incorrect information. (See 18 U.S.C. 1001)
+              </div>
             </div>
           </div>
         </div>
