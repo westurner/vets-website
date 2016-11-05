eb77259dbab2a2c004f59f9a3c2e700f53e8c11a
Courtney Eimerman-Wallace <crwallace39@gmail.com>
2016-11-05 16:31:01 -0400

    adds 508 test for login

---
+++ test/accessibility/login-508.spec.js
@@ -0,0 +1,13 @@
+const E2eHelpers = require('../util/e2e-helpers');
+const Timeouts = require('../util/timeouts.js');
+
+module.exports = E2eHelpers.createE2eTest(
+  (client) => {
+    client
+      .url(`${E2eHelpers.baseUrl}/`)
+      .waitForElementVisible('body', Timeouts.normal)
+      .waitForElementVisible('#login-root', Timeouts.slow)
+      .axeCheck('document');
+
+    client.end();
+  });
